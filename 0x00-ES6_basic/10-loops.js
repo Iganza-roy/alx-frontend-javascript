3c1338f80b8469f2b5cc20c364dd291d69a08f01
Roy iganza Kibata <iganzaroy55@gmail.com>
2024-07-22 22:38:08 +0000

    Add 10-loops.js

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (let [idx , value] of array.entires()) {
+    t addarray[idx] = appendString + value;
+  }
+
+  return array;
+}
