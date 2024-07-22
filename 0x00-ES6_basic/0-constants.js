85b8f22e44e4986371a854b86b3ee9fdeece0871
Iganza-roy <iganzaroy55@gmail.com>
2024-07-22 22:29:12 +0300

    Add test file

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,9 +1,10 @@
+
 export function taskFirst() {
   const task = "I prefer const when I can.";
   return task;
 }
 
-export function getLast() {
+ export function getLast() {
   return " is okay";
 }
 
