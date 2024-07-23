47ec0f0a633374941ed59151532bd1fd1e3e0652
Roy iganza Kibata <iganzaroy55@gmail.com>
2024-07-23 10:00:05 +0000

    Update 8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -5,13 +5,11 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const currentYear = getCurrentYear();
-  
   const budget = {
     [`income-${currentYear}`]: income,
     [`gdp-${currentYear}`]: gdp,
-    [`capita-${currentYear}`]: capita
+    [`capita-${currentYear}`]: capita,
   };
 
   return budget;
 }
-
