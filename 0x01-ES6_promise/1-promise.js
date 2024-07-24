bd52cfc2a4aa971e9a8a9803da570b1b4d8b124a
Iganza-roy <iganzaroy55@gmail.com>
2024-07-24 14:19:04 +0300

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,3 +1,12 @@
 export default getFullResponseFromAPI(success) {
-
+    return new Promise((resolve, reject) =>{
+        if (success) {
+            return ({
+                status: 200,
+                body: 'Success',
+            })}
+            else {
+                return (new Error('The fake API is not working currently'))
+            }
+    })
 }
