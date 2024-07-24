b62376855587025719c0a82d2eacfbbd81b5756c
Iganza-roy <iganzaroy55@gmail.com>
2024-07-24 20:02:07 +0300

    Update 3-all.js

---
+++ 4-user-promise.js
@@ -0,0 +1,13 @@
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
+      resolve({
+        firstName,
+        lastName,
+      });
+    } else {
+      reject(new Error("Error"));
+    }
+  });
+}
