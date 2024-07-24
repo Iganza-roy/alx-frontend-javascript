49eea683b0eb211e2c0983887848160951be9179
Roy iganza Kibata <iganzaroy55@gmail.com>
2024-07-24 11:26:31 +0000

    Add 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
-export default getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) =>{
-        if (success) {
-            return ({
-                status: 200,
-                body: 'Success',
-            })}
-            else {
-                return (new Error('The fake API is not working currently'))
-            }
-    })
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      return ({
+        status: 200,
+        body: 'Success',
+      });
+    }
+
+    return (new Error('The fake API is not working currently'));
+  });
 }
