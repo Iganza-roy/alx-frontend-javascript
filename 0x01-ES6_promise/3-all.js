4d9bcfb2a047cf60501bdd429026016f9c21600f
Iganza-roy <iganzaroy55@gmail.com>
2024-07-24 19:38:23 +0300

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,2 +1,11 @@
+import { createUser, uploadPhoto } from "./utils.js";
 export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photo, user] = results;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
