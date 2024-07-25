733cf68765ff6e5b2fce91eb8c5aa8a3e0a7ab97
Iganza-roy <iganzaroy55@gmail.com>
2024-07-26 00:12:37 +0300

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,17 +2,21 @@ class Building {
   constructor(sqft) {
     if (typeof sqft !== "number") throw new Error();
     this._sqft = sqft;
+
+    if (
+      new.target !== Building &&
+      typeof this.evacuationWarningMessage !== "function"
+    ) {
+      throw new Error(
+        "Class extending Building must override evacuationWarningMessage"
+      );
+    }
   }
 
   get sqft() {
     return this._sqft;
   }
   evacuationWarningMessage() {
-    if (new.target === Building) {
-      throw new Error(
-        "Class extending Building must override evacuationWarningMessage"
-      );
-    }
     throw new Error("Method not implemented");
   }
 }
