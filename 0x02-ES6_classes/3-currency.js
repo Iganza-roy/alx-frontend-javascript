bd48a3330c19891e3dccbddef7a79ec25be2a47a
Roy iganza Kibata <iganzaroy55@gmail.com>
2024-07-25 20:22:57 +0000

    Update 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,7 +1,7 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof code === "string") throw new TypeError();
-    if (typeof name === "string") throw new TypeError();
+    if (typeof code === 'string') throw new TypeError();
+    if (typeof name === 'string') throw new TypeError();
 
     this._code = code;
     this._name = name;
@@ -14,12 +14,12 @@ export default class Currency {
   }
 
   set code(code) {
-    if (typeof code !== "string") throw new TypeError("Name must be a string");
+    if (typeof code !== 'string') throw new TypeError('Name must be a string');
     this._code = code;
   }
 
   set name(name) {
-    if (typeof name !== "string") throw new TypeError("Name must be a string");
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
     this._name = name;
   }
 
