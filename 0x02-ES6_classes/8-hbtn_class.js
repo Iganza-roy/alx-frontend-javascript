726da3e47bc8bd9340aa869959cae6d91ab99056
Roy iganza Kibata <iganzaroy55@gmail.com>
2024-07-25 22:02:32 +0000

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,7 +1,7 @@
 export default class HolbertonClass {
   constructor(size, location) {
-    if (typeof size !== "number") throw new TypeError();
-    if (typeof location !== "string") throw new TypeError();
+    if (typeof size !== 'number') throw new TypeError();
+    if (typeof location !== 'string') throw new TypeError();
     this._size = size;
     this._location = location;
   }
