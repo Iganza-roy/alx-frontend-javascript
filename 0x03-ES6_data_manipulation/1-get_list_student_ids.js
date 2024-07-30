e693e70f7c1358d4fa4bcc90549ff47d284b141e
Roy iganza Kibata <iganzaroy55@gmail.com>
2024-07-30 12:31:25 +0000

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,7 @@ function getListStudentIds(arr_obj) {
   if (!Array.isArray(arr_obj)) {
     return [];
   }
-  return arr_obj.map((student) => student.id);
+  return arr_obj.map(student => student.id);
 }
 
 export default getListStudentIds;
